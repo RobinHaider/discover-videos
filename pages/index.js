33dbabc01fe911abf0407391e2a3eab17a52fe5d
Robin Haider <robinhaider69@gmail.com>
2022-08-01 13:59:40 +0600

    showing videos data from json

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Banner from '../components/banner/banner'
-import Navbar from '../components/nav/navbar';
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import Banner from '../components/banner/banner';
+import NavBar from '../components/nav/navbar';
+import Card from '../components/card/card';
+import SectionCards from '../components/card/section-cards';
+
+import { getVideos } from '../lib/videos';
 
 export default function Home() {
+  const disneyVideos = getVideos();
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Netflix</title>
-        <meta
-          name='description'
-          content='Discover youtube videos a netflix clone'
-        />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Navbar/>
+      <NavBar username='ankita@ank.com' />
       <Banner
-        videoId='4zH5iYM4wJo'
         title='Clifford the red dog'
         subTitle='a very cute dog'
         imgUrl='/static/clifford.webp'
       />
+
+      <div className={styles.sectionWrapper}>
+        <SectionCards title='Disney' videos={disneyVideos} size='large' />
+        <SectionCards title='Disney' videos={disneyVideos} size='medium' />
+      </div>
     </div>
   );
 }
